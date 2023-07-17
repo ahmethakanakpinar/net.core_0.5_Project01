5ecd2afcd40796f924f06f87c4960bf115b1de12
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2023-07-17 16:39:31 +0300

    index i View Components lere ayırıyorum Feature ve Headtamamlandı

---
+++ Core_Proje1/ViewComponents/Feature/FeatureList.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje1.ViewComponents.Feature
+{
+    public class FeatureList : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View(); 
+        }
+    }
+}
---
+++ Core_Proje1/ViewComponents/Head/HeadList.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Core_Proje1.ViewComponents.Head
+{
+    public class HeadList : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
