2d0a0df9e1971e47ac6b86af94b74c3278e35823
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2023-07-17 17:03:38 +0300

    header kısmı parçalandı ve Türkçeleştirildi

---
+++ Core_Proje1/ViewComponents/Header/HeaderList.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje1.ViewComponents.Header
+{
+    public class HeaderList : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View(); 
+        }
+    }
+}
