0f6d431cf79948bc7b874797daaa52c9428fedd1
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2023-07-18 11:50:36 +0300

    Portfolio kısmının gereksiz olduğu düşünüldü ve tamamen silindi

--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -19,7 +19,6 @@ namespace DataAccessLayer.Concrete
         public DbSet<Experience> Experiences { get; set; }
         public DbSet<Feature> Features { get; set; }
         public DbSet<Message> Messages { get; set; }
-        public DbSet<Portfolio> Portfolios { get; set; }
         public DbSet<Service> Services { get; set; }
         public DbSet<Skill> Skills { get; set; }
         public DbSet<SocialMedia> SocialMedias { get; set; }
