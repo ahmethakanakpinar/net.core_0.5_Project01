bbd9d93219944449d962be6bb4731d0cc0b5d4de
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2023-07-18 11:34:04 +0300

    Skill kısmı bölündü skill kısmı düzeltildi

--- Core_Proje1/ViewComponents/Skill/SkillList.cs
+++ Core_Proje1/ViewComponents/Skill/SkillList.cs
@@ -1,12 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Core_Proje1.ViewComponents.Skill
 {
     public class SkillList : ViewComponent
     {
+        SkillManager skillManager = new SkillManager(new EfSkillDal());
         public IViewComponentResult Invoke()
         {
-            return View();
+            var values = skillManager.TGetList();
+            return View(values);
         }
     }
 }
