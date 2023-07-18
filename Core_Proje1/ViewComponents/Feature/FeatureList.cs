8e2deddd4c295646680eeffafa6faf0b53649ec5
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2023-07-18 10:25:42 +0300

    Feature viewcomponents de veritabanı bağlantısı yazıldı view de ise veritabanından veri çekildi

--- Core_Proje1/ViewComponents/Feature/FeatureList.cs
+++ Core_Proje1/ViewComponents/Feature/FeatureList.cs
@@ -1,12 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Core_Proje1.ViewComponents.Feature
 {
     public class FeatureList : ViewComponent
     {
+        FeatureManager featureManager = new FeatureManager(new EfFeatureDal());
         public IViewComponentResult Invoke()
         {
-            return View(); 
+            var values = featureManager.TGetList();
+            return View(values); 
         }
     }
 }
