2c2d2a925a00641cdabb25a0004be554990bc229
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2023-07-18 10:41:15 +0300

    About kısmını böldüm ve düzenledim

--- Core_Proje1/ViewComponents/About/AboutList.cs
+++ Core_Proje1/ViewComponents/About/AboutList.cs
@@ -1,12 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Core_Proje1.ViewComponents.About
 {
     public class AboutList : ViewComponent
     {
+        AboutManager aboutManager = new AboutManager(new EfAboutDal());
         public IViewComponentResult Invoke()
-        { 
-            return View(); 
+        {
+            var values = aboutManager.TGetList();
+            return View(values); 
         }
     }
 }
