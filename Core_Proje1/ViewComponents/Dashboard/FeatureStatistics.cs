d40063134ed66e850168b4a3ab9f68d113367706
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2023-08-21 16:35:32 +0300

    Dashboard Istatislik kısmı tamamlandı

---
+++ Core_Proje1/ViewComponents/Dashboard/FeatureStatistics.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje1.ViewComponents.Dashboard
+{
+    public class FeatureStatistics : ViewComponent
+    {
+        Context c = new Context();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.SkillCount = c.Skills.Count();
+            ViewBag.NotReadMessagesCount = c.Messages.Where(x => x.Status == false).Count();
+            ViewBag.ReadMessagesCount = c.Messages.Where(x => x.Status == true).Count();
+            ViewBag.ExperiencesCount = c.Experiences.Count();
+            return View();
+        }
+    }
+}
