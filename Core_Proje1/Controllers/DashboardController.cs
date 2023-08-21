336df05c186fe3b6341bb8a0023b21c1ee8ebb4f
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2023-08-21 15:29:02 +0300

    dashboard kısmı oluşturuldu ve tasarımı eklendi

---
+++ Core_Proje1/Controllers/DashboardController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje1.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.default1 = "Dashboard";
+            ViewBag.default2 = "Dashboard";
+            ViewBag.default3 = "Dashboard";
+            return View();
+        }
+    }
+}
