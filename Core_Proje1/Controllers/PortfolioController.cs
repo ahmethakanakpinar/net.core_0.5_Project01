fff10407e1f6ee21f9e6abf6c871ad823a01b3d4
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2023-08-22 05:04:14 +0300

    portfolio list kısmı yapıldı

---
+++ Core_Proje1/Controllers/PortfolioController.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje1.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        PortfolioManager portfolioManager = new PortfolioManager(new EfPortfolioDal());
+        public IActionResult Index()
+        {
+            ViewBag.default1 = "Liste";
+            ViewBag.default2 = "Portfolio";
+            ViewBag.default3 = "Portfolio";
+            var values = portfolioManager.TGetList();
+            return View(values);
+        }
+    }
+}
