facb8a880afa356fddb71ac624dd70327230143d
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2023-10-10 10:57:38 +0300

    Writer Profile Güncelleme işlemi yapıldı

--- Core_Proje1/Areas/Writer/Controllers/ProfileController.cs
+++ Core_Proje1/Areas/Writer/Controllers/ProfileController.cs
@@ -1,4 +1,5 @@
 ﻿using BusinessLayer.Concrete;
+using Core_Proje1.Areas.Writer.Models;
 using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -14,11 +15,38 @@ namespace Core_Proje1.Areas.Writer.Controllers
         {
             _userManager = userManager;
         }
-
+        [HttpGet]
         public async Task<IActionResult> Index()
         {
             var values = await _userManager.FindByNameAsync(User.Identity.Name);
-            return View(values);
+            UserEditViewModel model = new UserEditViewModel();
+            model.Name = values.Name;
+            model.Surname = values.Surname;
+            model.PictureUrl = values.ImageUrl;
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(UserEditViewModel p)
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            if (p.Picture != null) 
+            {
+                var resource = Directory.GetCurrentDirectory();
+                var extension = Path.GetExtension(p.Picture.FileName);
+                var imagename = Guid.NewGuid() + extension;
+                var savelocation = resource + "/wwwroot/userimage/" + imagename;
+                var stream = new FileStream(savelocation, FileMode.Create);
+                await p.Picture.CopyToAsync(stream);
+                user.ImageUrl = imagename;
+            }
+            user.Name = p.Name;
+            user.Surname = p.Surname;
+            var result = await _userManager.UpdateAsync(user);
+            if (result.Succeeded)
+            {
+                return Redirect("/Writer/Profile/Index");
+            }
+            return View();
         }
     }
 }
---
+++ Core_Proje1/Areas/Writer/Models/UserEditViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Core_Proje1.Areas.Writer.Models
+{
+    public class UserEditViewModel
+    {
+        public string Name { get; set; }
+        public string Surname {  get; set; }
+        public string Password { get; set; }
+        public string PasswordConfirm { get; set; }
+        public string PictureUrl { get; set; }
+        public IFormFile Picture { get; set; }
+
+    }
+}
