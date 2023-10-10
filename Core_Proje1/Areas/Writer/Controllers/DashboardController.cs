3b358e1b8f7c6602503ad1f8aa30a694a9b4ed31
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2023-10-10 17:38:08 +0300

    Dashboard ve Profile de çalışmayan scriptler writerlayout a rendersection eklenerek düzeltildi

---
+++ Core_Proje1/Areas/Writer/Controllers/DashboardController.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLayer.Concrete;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje1.Areas.Writer.Controllers
+{
+    [Area("Writer")]
+    public class DashboardController : Controller
+    {
+        private readonly UserManager<WriterUser> _userManager;
+
+        public DashboardController(UserManager<WriterUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.v = values.Name + " " + values.Surname;
+            return View();
+        }
+    }
+}
