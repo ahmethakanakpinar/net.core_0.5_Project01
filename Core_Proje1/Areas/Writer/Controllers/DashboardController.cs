08dea1259d4cac629c0f46bb20a4bc61e96e69e3
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2023-10-13 14:55:42 +0300

    writer dashboard view için hava durumu api si çekildi

--- Core_Proje1/Areas/Writer/Controllers/DashboardController.cs
+++ Core_Proje1/Areas/Writer/Controllers/DashboardController.cs
@@ -3,6 +3,7 @@ using DataAccessLayer.Concrete;
 using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
 
 namespace Core_Proje1.Areas.Writer.Controllers
 {
@@ -20,6 +21,12 @@ namespace Core_Proje1.Areas.Writer.Controllers
         {
             var values = await _userManager.FindByNameAsync(User.Identity.Name);
             ViewBag.v = values.Name + " " + values.Surname;
+
+            string api = "5aaa6b07618b4702d2ccf1782148771d";
+            string connection = "https://api.openweathermap.org/data/2.5/weather?q=istanbul&mode=xml&units=metric&appid=" + api;
+            XDocument document = XDocument.Load(connection);
+            ViewBag.weather = document.Descendants("temperature").ElementAt(0).Attribute("value").Value;
+
             Context c = new Context();
             ViewBag.v1 = 0;
             ViewBag.v2 = c.Announcements.Count();
