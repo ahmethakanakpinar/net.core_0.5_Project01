7d8d325a7ecce682296f0cc678ca9058214d2a17
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2023-10-16 10:49:59 +0300

    Bildirimler Kısmı Writer Dashboard a eklendi

---
+++ Core_Proje1/Areas/Writer/ViewComponents/Notification/NotificationList.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLayer.Abstract;
+using BusinessLayer.Concrete;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje1.Areas.Writer.ViewComponents.Notification
+{
+    public class NotificationList : ViewComponent
+    {
+        AnnouncementManager _AnnouncementManager = new AnnouncementManager(new EfAnnouncementDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = _AnnouncementManager.TGetList();
+            return View(values);
+        }
+    }
+}
