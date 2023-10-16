8d97fcd5b3877a5f2c55df7b89a175cbc0384d53
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2023-10-16 14:08:53 +0300

    Writer UserImage ViewComponent ile eklendi

---
+++ Core_Proje1/Areas/Writer/ViewComponents/UserImage/UserImageList.cs
@@ -0,0 +1,24 @@
+﻿using BusinessLayer.Concrete;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje1.Areas.Writer.ViewComponents.UserImage
+{
+    public class UserImageList: ViewComponent
+    {
+        private readonly UserManager<WriterUser> _userManager;
+
+        public UserImageList(UserManager<WriterUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.UserImage = values.ImageUrl;
+            return View();
+        }
+    }
+}
