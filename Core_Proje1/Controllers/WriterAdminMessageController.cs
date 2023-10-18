0c55994a8efe81471b953b18a5a9fba5ff89bea5
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2023-10-18 10:16:25 +0300

    WriterAdmin Message temel atıldı Controller kısmı yazıldı

---
+++ Core_Proje1/Controllers/WriterAdminMessageController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje1.Controllers
+{
+    public class WriterAdminMessageController : Controller
+    {
+        public IActionResult ReceiverMessageList()
+        {
+            return View();
+        }
+        public IActionResult SenderMessageList()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult SendMessage()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult SendMessage(int i)
+        {
+            return View();
+        }
+    }
+}
